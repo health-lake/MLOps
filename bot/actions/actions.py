bdd22aed494a656595c3e1e148063fa8c656d7d9
Marcelo <marcelofelippe.mfg@gmail.com>
2021-07-09 17:19:12 -0300

    Consertando problema com as actions customizadas

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -4,16 +4,6 @@
 # Veja o guia na documentação do RASA em:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import unicode_literals
-#import numpy as np
-#from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-#from sklearn.svm import LinearSVC
-#from sklearn import metrics
-#from sklearn.model_selection import train_test_split
-#from sklearn.pipeline import Pipeline
-import pandas as pd
 from joblib import load
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
@@ -23,47 +13,6 @@ from rasa_sdk.events import SlotSet
 import requests
 
 from random import randint
-
-def limpa_texto(df_texto):
-    df_texto['nv_Texto'] = df_texto['Texto'].copy()
-    df_texto['nv_Texto'] = df_texto['nv_Texto'].str.replace('[,.:;!?]+', ' ', regex=True).copy()
-    df_texto['nv_Texto'] = df_texto['nv_Texto'].str.replace('[/<>()|\+\-\$%&#@\'\"]+', ' ', regex=True).copy()
-    df_texto['nv_Texto'] = df_texto['nv_Texto'].str.replace('[0-9]+', '', regex=True)
-    return df_texto    
-   
-class ActionClassificador(Action):
-
-    def name(self) -> Text:
-        return 'action_classificador'
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ) -> List[Dict[Text, Any]]:
-            
-        df = pd.read_csv('./classificador/catalogo.csv',delimiter=';')
-        df_classificador = df[['Classificação','Título','Link']].drop_duplicates().reset_index(drop=True).copy()
-        classificador = load('./classificador/classificador.joblib')  
-        
-        msg = (tracker.latest_message)['text']
-        Texto = [{'Texto': msg}]
-        #print(Texto)
-        str_result = 'Nenhum dado relevante informado'
-
-        if not msg == '':
-            df_texto = pd.DataFrame(Texto)
-            df_texto = limpa_texto(df_texto)
-            
-            classifica = classificador.predict(df_texto['nv_Texto'])[0]
-            titulo = df_classificador.query('Classificação == @classifica')['Título'].values[0]
-            link = df_classificador.query('Classificação == @classifica')['Link'].values[0]
-            str_result = 'Título: ' + titulo + ', no link: ' + link
-
-        dispatcher.utter_message(str_result)
-
-        return []
 
 class ActionTeste(Action):
     def name(self) -> Text:
